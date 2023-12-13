1e03c9bfc08d11b6f9b2cb7d872c7be09f000ee7
samiksha-3010 <samiksha30102@gmail.com>
2023-12-13 16:42:30 +0530

    done

---
+++ LeetCode.js
@@ -0,0 +1,11 @@
+// var removeDupilcates = function(nums){
+//       var number = 1;
+//       for(var i=0; i<nums.length-1;i++){
+//           if(nums [i] !== [i+1]){
+//               nums [number] = nums [i+1]
+//               number++;
+//           }
+//       }
+//       return number
+//   }
+//   console.log( removeDupilcates([0,0,1,1,1,2,2,3,3,4]))
